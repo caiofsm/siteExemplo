7ee1902a642c49d24852c6b369c938ea22c0b041
Caio <caio.fsm@gmail.com>
2022-05-07 02:55:59 -0300

    add efeito de transicao

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+const delay = ms => new Promise(res => setTimeout(res, ms));
+
 const container = document.getElementById("main-content");
 
 const linksMenu = document.querySelectorAll('.menu-list a')
 
-//console.log(linksMenu)
+const html = document.querySelector('html');
+// console.log(linksMenu)
 
 linksMenu.forEach(elemento =>
 {
@@ -15,16 +18,20 @@ function carregarPagina(pagina)
     fetch('pages/' + pagina)
         .then(async function (response)
         {
-            
             container.insertAdjacentHTML('afterbegin', (await response.text()))
+            container.children[0].style.opacity = 0;
+            await delay(300);
+            container.children[0].style.opacity = 1;
         });
 }
 
-function trocarDePagina(pagina, elemento)
+async function trocarDePagina(pagina, elemento)
 {
-    container.removeChild(container.children[0]);
+    container.children[0].style.opacity = 0;
     linksMenu.forEach(a => { a.classList.remove('is-active') })
     elemento.classList.add('is-active')
+    await delay(300)
+    container.removeChild(container.children[0]);
     carregarPagina(pagina);
 }
 
